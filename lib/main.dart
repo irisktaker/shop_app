515430c6f2b9b99d5faf77ac1fcef2de76eea546
Naser Sami <ponanzar@gmail.com>
2022-02-09 08:21:50 +0200

    shop_app

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shop_app/constants.dart';
+import 'screens/home/home_screen.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ),
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Shop app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: bgColor,
+        fontFamily: 'Gordita',
+        textTheme: const TextTheme(
+          bodyText2: TextStyle(
+            color: Colors.black54,
+          ),
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
